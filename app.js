fdbfaaa395560dc8213e974f8bbd193d1048511a
Nikos Rossolatos <spidey_nr@hotmail.com>
2015-07-03 16:42:25 +0300

    added surge- events , join,leave,joined

--- app.js
+++ app.js
@@ -12,11 +12,30 @@ var util = require('util');
 var io = require('socket.io')(8080);
 
 io.on('connection', function (socket) {
-	console.log('socket connected for client')
+	console.log('socket connected for client');
+
 	socket.on('message', function (message) {
-		console.log('message sent :  '+message);
-		socket.broadcast.emit('message','pong');
+
+		console.log('message received : '+JSON.stringify(message));
+
+		if(message.event=='join_room'){
+			socket.join(message.room);
+			socket.emit('message',{event:'surge-joined_room',room:message.room});
+		}
+		if(message.event=='leave_room'){
+			socket.leave(message.room);
+			socket.emit('message',{event:'surge-left_room',room:message.room});
+		}
+		if(message.event=='joined_rooms'){
+			socket.emit('message',{message:'rooms connected : '+socket.rooms})
+		}
+		else{
+			message.message = 'pong';
+			socket.broadcast.emit('message',message);
+		}
+		
 	});
+
 	socket.on('disconnect', function () {
 		console.log('socket disconnected for client')
 	});
